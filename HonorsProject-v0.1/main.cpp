c96ea2c5c6d99dd350653d3ca399d9cfcf8e24b9
Yeffry-Fermin <anthonybatista291@gmail.com>
2026-04-23 17:26:52 -0400

    update menu to hold further options

--- HonorsProject-v0.1/main.cpp
+++ HonorsProject-v0.1/main.cpp
@@ -4,19 +4,21 @@
 #include <limits>
 #include "MoodEntry.h"
 #include "AppController.h"
+#include "AnalyticsEngine.h"
 
 int main() {;
     DatabaseManager globalDb;
-    AppController controller(globalDb); // Pass it in
+    AnalyticsEngine analytics;
+    AppController controller(globalDb, analytics);
     
     bool isMainMenuActive = true;
     int input;
     
     while(isMainMenuActive) {
         std::cout << "\n==== Vitality Hub ====\n"
-        << "1. Log Mood Entry(Work In progress)\n"
-        << "2. View History(TBA)\n"
-        << "3. Analytics(TBA)\n"
+        << "1. Log Mood Entry\n" // DONE
+        << "2. View History\n" // DONE
+        << "3. Analytics(TBA)\n" // WORK IN PROGRESS
         << "4. Breathing Space(Not part of MVP)\n"
         << "5. Exit \n";
         //add input validation later for now this is ok as MVP
@@ -38,15 +40,47 @@ int main() {;
                 << "4. Correlation Analysis (TBA)\n"
                 << "5. Back to Main Menu\n";
                 
-                bool isAnalyticsActive = true;
+                bool isAnalyticsMenuActive = true;
                 int analyticsInput;
                 
-                while(isAnalyticsActive) {
-                    cin >> analyticsInput;
+                while(isAnalyticsMenuActive) {
+                    std::cin >> analyticsInput;
                     
                     switch (analyticsInput) {
-                        case 1: // function to handle mood averages
+                            
+                        case 1: {
+                            bool isMoodAvgMenuActive = true;
+                            int moodInputOption;
+                            
+                            while(isMoodAvgMenuActive) {
+                                std::cout << "\n==== Mood Averages ====\n"
+                                << "1. Last 7 Days\n"
+                                << "2. Last 30 Days\n"
+                                << "3. All Time\n"
+                                << "4. Back to Analytics Menu\n";
+                                
+                                std::cin >> moodInputOption;
+                                
+                                switch (moodInputOption) {
+                                    case 1:
+                                        // Show last 7 days averages
+                                        break;
+                                    case 2:
+                                        // Show last 30 days averages
+                                        break;
+                                    case 3:
+                                        // Show average from all time
+                                    case 4:
+                                        moodInputOption = false;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                            
                             break;
+                        }
+                            
                         case 2: // function to handle trigger frequencies
                             break;
                         case 3: // TBA
@@ -54,7 +88,7 @@ int main() {;
                         case 4: // TBA
                             break;
                         case 5: // Go back to previous menu
-                            isAnalyticsActive = false;
+                            isAnalyticsMenuActive = false;
                             break;
                         default:
                             break;
